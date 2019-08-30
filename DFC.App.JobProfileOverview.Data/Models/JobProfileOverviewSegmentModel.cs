16ee5a3323af2371be49c42999d57cf054aa9af8
ilyasdhin <ilyasdhin@gmail.com>
2019-08-30 10:55:01 +0100

    Added cosmos stuff and updated packages (#1)

---
+++ DFC.App.JobProfileOverview.Data/Models/JobProfileOverviewSegmentModel.cs
@@ -0,0 +1,21 @@
+﻿using DFC.App.JobProfileOverview.Data.Contracts;
+using Newtonsoft.Json;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DFC.App.JobProfileOverview.Data.Models
+{
+    public class JobProfileOverviewSegmentModel:IDataModel
+    {
+        [JsonProperty(PropertyName = "id")]
+        public Guid DocumentId { get; set; }
+
+        [Required]
+        public string CanonicalName { get; set; }
+
+        public string Content { get; set; }
+
+        [Display(Name = "Last Reviewed")]
+        public DateTime LastReviewed { get; set; }
+    }
+}
