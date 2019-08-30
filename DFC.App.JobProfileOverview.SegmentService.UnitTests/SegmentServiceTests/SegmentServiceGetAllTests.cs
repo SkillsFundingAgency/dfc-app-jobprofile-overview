fcaa20e1478b19d341161895eff9a621b57e23c9
Ilyas Dhin <ilyasdhin@gmail.com>
2019-08-30 15:56:59 +0100

    Added tests

---
+++ DFC.App.JobProfileOverview.SegmentService.UnitTests/SegmentServiceTests/SegmentServiceGetAllTests.cs
@@ -0,0 +1,55 @@
+﻿using DFC.App.JobProfileOverview.Data.Contracts;
+using DFC.App.JobProfileOverview.Data.Models;
+using FakeItEasy;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DFC.App.JobProfileOverview.SegmentService.UnitTests.SegmentServiceTests
+{
+    public class SegmentServiceGetAllTests
+    {
+        private readonly ICosmosRepository<JobProfileOverviewSegmentModel> repository;
+        private readonly IDraftJobProfileOverviewSegmentService draftJobProfileOverviewSegmentService;
+        private readonly IJobProfileOverviewSegmentService jobProfileOverviewSegmentService;
+
+        public SegmentServiceGetAllTests()
+        {
+            repository = A.Fake<ICosmosRepository<JobProfileOverviewSegmentModel>>();
+            draftJobProfileOverviewSegmentService = A.Fake<IDraftJobProfileOverviewSegmentService>();
+            jobProfileOverviewSegmentService = new JobProfileOverviewSegmentService(repository, draftJobProfileOverviewSegmentService);
+        }
+
+        [Fact]
+        public async Task GetAllListReturnsSuccess()
+        {
+            // arrange
+            var expectedResults = A.CollectionOfFake<JobProfileOverviewSegmentModel>(2);
+
+            A.CallTo(() => repository.GetAllAsync()).Returns(expectedResults);
+
+            // act
+            var results = await jobProfileOverviewSegmentService.GetAllAsync().ConfigureAwait(false);
+
+            // assert
+            A.CallTo(() => repository.GetAllAsync()).MustHaveHappenedOnceExactly();
+            A.Equals(results, expectedResults);
+        }
+
+        [Fact]
+        public async Task GetAllListReturnsNullWhenMissingRepository()
+        {
+            // arrange
+            IEnumerable<JobProfileOverviewSegmentModel> expectedResults = null;
+
+            A.CallTo(() => repository.GetAllAsync()).Returns(expectedResults);
+
+            // act
+            var results = await jobProfileOverviewSegmentService.GetAllAsync().ConfigureAwait(false);
+
+            // assert
+            A.CallTo(() => repository.GetAllAsync()).MustHaveHappenedOnceExactly();
+            A.Equals(results, expectedResults);
+        }
+    }
+}
