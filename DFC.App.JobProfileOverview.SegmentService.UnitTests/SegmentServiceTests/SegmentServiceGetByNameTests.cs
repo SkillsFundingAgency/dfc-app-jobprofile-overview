6036bc1d5c4dc8d28abd94d459e0d5e1d914ff57
Ilyas Dhin <ilyasdhin@gmail.com>
2019-09-02 08:37:50 +0100

    Added tests for get by Id, name and all

---
+++ DFC.App.JobProfileOverview.SegmentService.UnitTests/SegmentServiceTests/SegmentServiceGetByNameTests.cs
@@ -0,0 +1,69 @@
+﻿using DFC.App.JobProfileOverview.Data.Contracts;
+using DFC.App.JobProfileOverview.Data.Models;
+using FakeItEasy;
+using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DFC.App.JobProfileOverview.SegmentService.UnitTests.SegmentServiceTests
+{
+    public class SegmentServiceGetByNameTests
+    {
+        private readonly ICosmosRepository<JobProfileOverviewSegmentModel> repository;
+        private readonly IDraftJobProfileOverviewSegmentService draftJobProfileOverviewSegmentService;
+        private readonly IJobProfileOverviewSegmentService jobProfileOverviewSegmentService;
+
+        public SegmentServiceGetByNameTests()
+        {
+            repository = A.Fake<ICosmosRepository<JobProfileOverviewSegmentModel>>();
+            draftJobProfileOverviewSegmentService = A.Fake<IDraftJobProfileOverviewSegmentService>();
+            jobProfileOverviewSegmentService = new JobProfileOverviewSegmentService(repository, draftJobProfileOverviewSegmentService);
+        }
+
+        [Fact]
+        public async Task SegmentServiceGetByNameReturnsSuccess()
+        {
+            // arrange
+            var documentId = Guid.NewGuid();
+            var expectedResult = A.Fake<JobProfileOverviewSegmentModel>();
+
+            A.CallTo(() => repository.GetAsync(A<Expression<Func<JobProfileOverviewSegmentModel, bool>>>.Ignored)).Returns(expectedResult);
+
+            // act
+            var result = await jobProfileOverviewSegmentService.GetByNameAsync("article-name").ConfigureAwait(false);
+
+            // assert
+            A.CallTo(() => repository.GetAsync(A<Expression<Func<JobProfileOverviewSegmentModel, bool>>>.Ignored)).MustHaveHappenedOnceExactly();
+            A.Equals(result, expectedResult);
+        }
+
+        [Fact]
+        public async Task SegmentServiceGetByNameReturnsArgumentNullExceptionWhenNullIsUsed()
+        {
+            // arrange
+
+            // act
+            var exceptionResult = await Assert.ThrowsAsync<ArgumentNullException>(async () => await jobProfileOverviewSegmentService.GetByNameAsync(null).ConfigureAwait(false)).ConfigureAwait(false);
+
+            // assert
+            Assert.Equal("Value cannot be null.\r\nParameter name: canonicalName", exceptionResult.Message);
+        }
+
+        [Fact]
+        public async Task SegmentServiceGetByNameReturnsNullWhenMissingInRepository()
+        {
+            // arrange
+            JobProfileOverviewSegmentModel expectedResult = null;
+
+            A.CallTo(() => repository.GetAsync(A<Expression<Func<JobProfileOverviewSegmentModel, bool>>>.Ignored)).Returns(expectedResult);
+
+            // act
+            var result = await jobProfileOverviewSegmentService.GetByNameAsync("article-name").ConfigureAwait(false);
+
+            // assert
+            A.CallTo(() => repository.GetAsync(A<Expression<Func<JobProfileOverviewSegmentModel, bool>>>.Ignored)).MustHaveHappenedOnceExactly();
+            A.Equals(result, expectedResult);
+        }
+    }
+}
