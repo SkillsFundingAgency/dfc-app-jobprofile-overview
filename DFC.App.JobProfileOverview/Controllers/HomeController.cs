2904b7a1cc99a867d28096013766c7c5e2a2f8d9
Ilyas Dhin <ilyasdhin@gmail.com>
2019-09-05 09:24:51 +0100

    Misc fixes

--- DFC.App.JobProfileOverview/Controllers/HomeController.cs
+++ DFC.App.JobProfileOverview/Controllers/HomeController.cs
@@ -14,16 +14,6 @@ namespace DFC.App.JobProfileOverview.Controllers
             this.logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
