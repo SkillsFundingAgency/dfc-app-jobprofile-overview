b24254ca2fed55324fe9463b4e8cddd530a54bbe
Ilyas Dhin <ilyasdhin@gmail.com>
2019-09-13 09:43:00 +0100

    Added delete tests

---
+++ DFC.App.JobProfileOverview.UnitTests/ControllerTests/SegmentControllerTests/SegmentControllerDeleteTests.cs
@@ -0,0 +1,61 @@
+﻿using DFC.App.JobProfileOverview.Data.Models;
+using FakeItEasy;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using Xunit;
+
+namespace DFC.App.JobProfileOverview.UnitTests.ControllerTests.SegmentControllerTests
+{
+    public class SegmentControllerDeleteTests : BaseSegmentController
+    {
+        [Theory]
+        [MemberData(nameof(JsonMediaTypes))]
+        public async void SegmentControllerDeleteReturnsSuccess(string mediaTypeName)
+        {
+            // Arrange
+            var documentId = Guid.NewGuid();
+            var expectedResult = A.Fake<JobProfileOverviewSegmentModel>();
+            expectedResult.DocumentId = documentId;
+            var controller = BuildSegmentController(mediaTypeName);
+
+            A.CallTo(() => FakeJobProfileSegmentService.GetByIdAsync(documentId)).Returns(expectedResult);
+
+            // Act
+            var result = await controller.Delete(documentId).ConfigureAwait(false);
+
+            // Assert
+            A.CallTo(() => FakeJobProfileSegmentService.DeleteAsync(documentId)).MustHaveHappenedOnceExactly();
+
+            var okResult = Assert.IsType<OkResult>(result);
+
+            A.Equals((int)HttpStatusCode.OK, okResult.StatusCode);
+
+            controller.Dispose();
+        }
+
+        [Theory]
+        [MemberData(nameof(JsonMediaTypes))]
+        public async void SegmentControllerDeleteReturnsNotFound(string mediaTypeName)
+        {
+            // Arrange
+            var documentId = Guid.NewGuid();
+            JobProfileOverviewSegmentModel expectedResult = null;
+            var controller = BuildSegmentController(mediaTypeName);
+
+            A.CallTo(() => FakeJobProfileSegmentService.GetByIdAsync(documentId)).Returns(expectedResult);
+
+            // Act
+            var result = await controller.Delete(documentId).ConfigureAwait(false);
+
+            // Assert
+            A.CallTo(() => FakeJobProfileSegmentService.DeleteAsync(documentId)).MustHaveHappenedOnceExactly();
+
+            var statusResult = Assert.IsType<NotFoundResult>(result);
+
+            A.Equals((int)HttpStatusCode.NotFound, statusResult.StatusCode);
+
+            controller.Dispose();
+        }
+    }
+}
