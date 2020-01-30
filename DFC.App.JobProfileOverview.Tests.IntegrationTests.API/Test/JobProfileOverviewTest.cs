212f8db62de0c5c4b5eea6c3af44e27481aa5eb5
James Barnes <james@testtrends.co.uk>
2020-01-30 09:21:08 +0000

    Working hours update test working.

--- DFC.App.JobProfileOverview.Tests.IntegrationTests.API/Test/JobProfileOverviewTest.cs
+++ DFC.App.JobProfileOverview.Tests.IntegrationTests.API/Test/JobProfileOverviewTest.cs
@@ -29,15 +29,14 @@ namespace DFC.App.JobProfileOverview.Tests.IntegrationTests.API.Test
         [Description("Tests that the CType 'WorkingHoursDetails' successfully tiggers an update to an existing job profile")]
         public async Task JobProfileOverview_WorkingHoursDetails()
         {
-            WorkingHoursDetailsClassification workingHoursDetailsClassification = CommonAction.Genr(JobProfile);
-            byte[] messageBody = CommonAction.ConvertObjectToByteArray(socCodeContentType);
-            Message message = CommonAction.CreateServiceBusMessage(socCodeContentType.Id, messageBody, ContentType.JSON, ActionType.Published, CType.JobProfileSoc);
+            WorkingHoursDetailsClassification workingHoursDetailsClassification = CommonAction.GenerateWorkingHoursDetailsClassificationForJobProfile(JobProfile);
+            byte[] messageBody = CommonAction.ConvertObjectToByteArray(workingHoursDetailsClassification);
+            Message message = CommonAction.CreateServiceBusMessage(workingHoursDetailsClassification.Id, messageBody, ContentType.JSON, ActionType.Published, CType.WorkingHoursDetails);
             await CommonAction.SendMessage(Topic, message);
             await Task.Delay(5000);
             string endpoint = Settings.APIConfig.EndpointBaseUrl.Replace("{id}", JobProfile.JobProfileId);
             Response<JobProfileOverviewAPIResponse> apiResponse = await CommonAction.ExecuteGetRequest<JobProfileOverviewAPIResponse>(endpoint, GetRequest.ContentType.Json);
-            Assert.AreEqual(socCodeContentType.SOCCode.Substring(0, 4), apiResponse.Data.soc);
-            Assert.AreEqual(socCodeContentType.ONetOccupationalCode, apiResponse.Data.oNetOccupationalCode);
+            Assert.AreEqual(workingHoursDetailsClassification.Title, apiResponse.Data.workingHoursDetails);
         }
     }
 }
