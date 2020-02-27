1b7bd53871a34e7a21a01f109941da7d6231450a
James Barnes <james@testtrends.co.uk>
2020-02-27 12:20:04 +0000

    Story/DFC-11481: Automated test pack (#89)
    * Added test project and common libraries.
    
    * Added test framework.
    
    * Test still not working for SOC Code.
    
    * Ready to make a call to the API for the SOC Code Data
    
    * WIP
    
    * WIP
    
    * Working hours update test working.
    
    * One additional test.
    
    * Finished!
    
    * Amended resource manager
    
    * Amended many warnings.
    
    * Fixed all local warnings.
    
    * Amended settings.
    
    * Removed underscore from method names.
    
    * Most warnings addressed.
    
    * Final warnings fixed.
    
    * Minor clean up of code.
    
    * Refactored the framework ready for unit tests.
    
    * Finished refactoring. Ready for the unit tests.
    
    * Minor amendments.
    
    * Removed common projects.
    
    * Added unit testing framework.
    
    * Started unit testing.
    
    * Pushing for Kader to take a look.
    
    * AppSettings class.
    
    * Added additional api tests.
    
    * Service bus and api tested.
    
    * Final amendments.
    
    * Removed redundant projects.
    
    * Addressed some warnings.
    
    * Addressed all Kaders comments.
    
    * Added app settings template and addressed warnings.
    
    * Missing settings.

---
+++ DFC.App.JobProfileOverview.Tests.IntegrationTests.API/Test/JobProfileOverviewTest.cs
@@ -0,0 +1,63 @@
+using DFC.App.JobProfileOverview.Tests.IntegrationTests.API.Support;
+using DFC.App.JobProfileOverview.Tests.IntegrationTests.API.Support.AzureServiceBus.ServiceBusFactory;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace DFC.App.JobProfileOverview.Tests.IntegrationTests.API.Test
+{
+    public class JobProfileOverviewTest : SetUpAndTearDown
+    {
+        [Test]
+        [Description("Tests that the CType 'JobProfileSoc' successfully tiggers an update to an existing job profile")]
+        public async Task JobProfileOverviewJobProfileSOC()
+        {
+            var socCodeContentType = this.CommonAction.GenerateSOCCodeContentTypeForJobProfile(this.JobProfile);
+            var messageBody = this.CommonAction.ConvertObjectToByteArray(socCodeContentType);
+            var message = new MessageFactory().Create(this.JobProfile.JobProfileId, messageBody, "Published", "JobProfileSoc");
+            await this.ServiceBus.SendMessage(message).ConfigureAwait(false);
+            await Task.Delay(5000).ConfigureAwait(false);
+            var apiResponse = await this.API.GetById(this.JobProfile.JobProfileId).ConfigureAwait(false);
+            Assert.AreEqual(socCodeContentType.SOCCode.Substring(0, 4), apiResponse.Data.SOC);
+            Assert.AreEqual(socCodeContentType.ONetOccupationalCode, apiResponse.Data.ONetOccupationalCode);
+        }
+
+        [Test]
+        [Description("Tests that the CType 'WorkingHoursDetails' successfully tiggers an update to an existing job profile")]
+        public async Task JobProfileOverviewWorkingHoursDetails()
+        {
+            var workingHoursDetailsClassification = this.CommonAction.GenerateWorkingHoursDetailsClassificationForJobProfile(this.JobProfile);
+            var messageBody = this.CommonAction.ConvertObjectToByteArray(workingHoursDetailsClassification);
+            var message = new MessageFactory().Create(this.JobProfile.JobProfileId, messageBody, "Published", "WorkingHoursDetails");
+            await this.ServiceBus.SendMessage(message).ConfigureAwait(false);
+            await Task.Delay(5000).ConfigureAwait(false);
+            var apiResponse = await this.API.GetById(this.JobProfile.JobProfileId).ConfigureAwait(false);
+            Assert.AreEqual(workingHoursDetailsClassification.Title, apiResponse.Data.WorkingHoursDetails);
+        }
+
+        [Test]
+        [Description("Tests that the CType 'WorkingPattern' successfully tiggers an update to an existing job profile")]
+        public async Task JobProfileOverviewWorkingPattern()
+        {
+            var workingPatternClassification = this.CommonAction.GenerateWorkingPatternClassificationForJobProfile(this.JobProfile);
+            var messageBody = this.CommonAction.ConvertObjectToByteArray(workingPatternClassification);
+            var message = new MessageFactory().Create(this.JobProfile.JobProfileId, messageBody, "Published", "WorkingPattern");
+            await this.ServiceBus.SendMessage(message).ConfigureAwait(false);
+            await Task.Delay(5000).ConfigureAwait(false);
+            var apiResponse = await this.API.GetById(this.JobProfile.JobProfileId).ConfigureAwait(false);
+            Assert.AreEqual(workingPatternClassification.Title, apiResponse.Data.WorkingPattern);
+        }
+
+        [Test]
+        [Description("Tests that the CType 'WorkingPatternDetails' successfully tiggers an update to an existing job profile")]
+        public async Task JobProfileOverviewWorkingPatternDetails()
+        {
+            var workingPatternDetailClassification = this.CommonAction.GenerateWorkingPatternDetailsClassificationForJobProfile(this.JobProfile);
+            var messageBody = this.CommonAction.ConvertObjectToByteArray(workingPatternDetailClassification);
+            var message = new MessageFactory().Create(this.JobProfile.JobProfileId, messageBody, "Published", "WorkingPatternDetails");
+            await this.ServiceBus.SendMessage(message).ConfigureAwait(false);
+            await Task.Delay(5000).ConfigureAwait(false);
+            var apiResponse = await this.API.GetById(this.JobProfile.JobProfileId).ConfigureAwait(false);
+            Assert.AreEqual(workingPatternDetailClassification.Title, apiResponse.Data.WorkingPatternDetails);
+        }
+    }
+}
