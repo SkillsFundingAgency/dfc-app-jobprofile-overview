9dcdfa902018f98059b9a39209f19e6d3b206d8b
kaderhashmi <52653766+kaderhashmi@users.noreply.github.com>
2020-03-10 08:45:19 +0000

    Migrate from Netcore 2.2 to 3.1

--- DFC.App.JobProfileOverview.Views.UnitTests/Services/RazorHtmlHelper.cs
+++ DFC.App.JobProfileOverview.Views.UnitTests/Services/RazorHtmlHelper.cs
@@ -1,12 +1,14 @@
-﻿using RazorEngine.Text;
+﻿ //This version of the package does not support Netcore 3.1 - removing until a suitable alternative view testing framework is used
 
-namespace DFC.App.JobProfileOverview.Views.UnitTests.Services
-{
-    public class RazorHtmlHelper
-    {
-        public IEncodedString Raw(string rawString)
-        {
-            return new RawString(rawString);
-        }
-    }
-}
+ //using RazorEngine.Text;
+
+//namespace DFC.App.JobProfileOverview.Views.UnitTests.Services
+//{
+//    public class RazorHtmlHelper
+//    {
+//        public IEncodedString Raw(string rawString)
+//        {
+//            return new RawString(rawString);
+//        }
+//    }
+//}
