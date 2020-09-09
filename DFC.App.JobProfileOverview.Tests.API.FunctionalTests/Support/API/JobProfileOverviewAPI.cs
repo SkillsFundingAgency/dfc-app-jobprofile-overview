e45b57f3b500c2eed0e2e9ab1f07d1568fe29548
James Barnes <james@testtrends.co.uk>
2020-09-09 12:15:09 +0100

    Amended endpoint

--- DFC.App.JobProfileOverview.Tests.API.FunctionalTests/Support/API/JobProfileOverviewAPI.cs
+++ DFC.App.JobProfileOverview.Tests.API.FunctionalTests/Support/API/JobProfileOverviewAPI.cs
@@ -27,7 +27,7 @@ namespace DFC.App.JobProfileOverview.Tests.IntegrationTests.API.Support.API
             }
 
             var restClient = this.restClientFactory.Create(this.appSettings.APIConfig.EndpointBaseUrl);
-            var restRequest = this.restRequestFactory.Create($"segment/{id}/contents");
+            var restRequest = this.restRequestFactory.Create($"/segment/{id}/contents");
             restRequest.AddHeader("Accept", "application/json");
             restRequest.AddHeader("Ocp-Apim-Subscription-Key", this.appSettings.APIConfig.ApimSubscriptionKey);
             restRequest.AddHeader("version", this.appSettings.APIConfig.Version);
